99c168c850c6ea30210f253c3cccc9baefdd3082
HarryStanley-afk <61549848+HarryStanley-afk@users.noreply.github.com>
2023-01-22 02:25:30 -0600

    Update Class2.java

--- Class2.java
+++ Class2.java
@@ -1,11 +1,13 @@
 /**
  *
- * @author Your Name
+ * @author Harry Stanley
  */
 
 public class Class2 {
 
-    private void f1() {
+    private void f1() 
+                 f2()
+                 f3(){
         // v1.0
     }
 
